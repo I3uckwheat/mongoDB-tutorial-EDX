f4611542dc3534d727b682c64cda27907ba2fccf
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-10 21:46:01 -0600

    adds inserting of documents

--- app.js
+++ app.js
@@ -6,5 +6,21 @@ const url = 'mongodb://localhost:27017/edx-course-db'
 MongoClient.connect(url, (err, db) => {
   if (err) return process.exit(1);
   console.log("You connected");
-  db.close()
+  insertDocuments(db, () => {
+    db.close()
+  })
 })
+
+function insertDocuments(db, callback){
+  const collection = db.collection('edx-course-students');
+
+  collection.insert([
+    {name: 'Bob'}, {name: "John"}, {name: "peter"}
+  ], (error, result) => {
+    if(error) return process.exit(1);
+    console.log(result.result.n);
+    console.log(result.ops.length);
+    console.log("Inserted 3 documents in teh edx course celection")
+    callback(result);
+  });
+}
